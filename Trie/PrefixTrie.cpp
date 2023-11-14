2e25592fad68857ed276880b7172e14548d120a3
Nasit Sarwar Sony <nasitsony96@gmail.com>
2023-11-14 15:18:10 -0800

    Update PrefixTrie.cpp
    added insert and search functinality

--- Trie/PrefixTrie.cpp
+++ Trie/PrefixTrie.cpp
@@ -17,4 +17,28 @@ class Trie{
     Trie(){
         root = new Node('\0');
     }
+
+    void insert(string word){
+        Node*temp = root;
+        for(char ch:word){
+            if(temp->m.count(ch) == 0){
+                Node* n = new Node(ch);
+                temp->m[ch] = n;
+            }
+            temp = temp->m[ch];
+        }
+        temp->isTerminal = true;
+    }
+    
+    bool search(string word){
+        Node*temp=root;
+        
+        for(char ch:word){
+            if(temp->m[ch]==0)
+                return false;
+            temp=temp->m[ch];
+        }
+        return temp->isTerminal;
+    }
+
 };
